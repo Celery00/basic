8c35875ad60333600d9e951c48c01a62d0833556
Celery00 <934740483@qq.com>
2018-09-18 09:36:32 +0800

    D:/Git/

---
+++ 移动端自定义联动/git_shell.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get current file path.
+CMD_PATH=`dirname $0`
+cd $CMD_PATH
+echo "current finder: $PWD"
+
+# Each finder files
+for filen in `ls $PWD`
+do
+	if [ $filen != 'git_shell.sh' ]
+	then
+		git add $filen
+		echo "git add $filen"
+	fi
+done
+
+# commit and push to master
+git commit -m "Add or update. -- git_shell.sh"
+git pull
+git push
